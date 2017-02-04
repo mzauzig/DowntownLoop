dcf134966acd912a6b3da8e89a3eacf8f49f1f78
Michael Zauzig <mzauzig@gmail.com>
2017-02-04 12:11:17 -0500

    Initial Commit

---
+++ DowntownLoop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DowntownLoop
+//
+//  Created by Michael Zauzig on 2/4/17.
+//  Copyright © 2017 Zoftworks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
